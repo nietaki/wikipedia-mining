cd61ed54b8a9039fb56bf818bc10c4540ff20173
Jacek Królikowski <nietaki@gmail.com>
2015-06-09 22:53:57 +0100

    more parsing problems discovered

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -1,6 +1,7 @@
 import ParsingStrategies._
+import analysis.TreeTraverser
 import com.github.tototoshi.csv.CSVWriter
-import utils.Config
+import utils.{Persistence, Config}
 
 /**
  * Created by nietaki on 05.01.15.
@@ -11,6 +12,8 @@ object Runner {
     println("starting")
     val articles = WikipediaDumpParser.getNonRedirectTitlesAndContents(Config.wikipediaXmlLocation)
 
+    // TODO: fix for articles like http://en.wikipedia.org/w/index.php?title=Mercedes-Benz_W21&action=edit (fix extraction from 
+    // TODO: remove all Wikipedia:Something articles
     val strategies: List[ParsingStrategy] = List(removeBrackets, removeParentsContainingLinks, extractFirstLink, removeHashFragment)
 
     articles.map(pr => pr match {
@@ -27,5 +30,17 @@ object Runner {
   def main(args: Array[String]): Unit = {
     //savePairsToCsv();
     //Persistence.printLineByLine(csvPath)
+    Persistence.readEntriesFromCsv(Config.pairsCsvLocation).filter(_.firstLink.length < 2).foreach(println(_))
+
+    return
+
+    var graph = TreeTraverser.buildGraphRepresentation(Persistence.readEntriesFromCsv(Config.pairsCsvLocation))
+    println(graph.size)
+    graph = graph.filter({case (k, properties) =>
+        properties.parentEntryNames.length > 10
+    })
+    graph.foreach({case (k, v) =>
+      val count = v.parentEntryNames.size
+      println(s"$k, $count")})
   }
 }
